9a013e6c2f7cde1b58354cd74280c9b7dbf93b55
roshkoff571 <74923347+roshkoff571@users.noreply.github.com>
2023-05-04 22:11:57 +0300

    стилей поменял

--- src/components/WorksPage.js
+++ src/components/WorksPage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import worksList from '../worksList'
+import animation from '../img/animation.gif'
 
 export default function WorksPage() {
   return (
-    <div className = 'worksPage'>
+    <div className = 'worksPage' style = {{backgroundImage: `url(${animation})`}}>
       {worksList.map((item, index) => {
-        return (<h2>{item.name}</h2>)
+        return (
+        <div className = 'workCard'>
+          <h3 key = {index}>{item.name}</h3>
+        </div>
+        )
       })}
     </div>
   )
